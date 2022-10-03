7ff22d5e74dd22fc7875f5e434cb4a083a57cc3c
Raymond Ha <Haro@mail.uc.edu>
2022-10-03 00:35:01 -0400

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       int ans, guess, guessNum = 0;
+       final int maxGuesses = 5;
+       String str, playAgain = "y";
+
+       Scanner scan = new Scanner(System.in);
+       Random generator = new Random();
+       ans = generator.nextInt(10) + 1;
+
+       while (playAgain.equals("y") || playAgain.equals("Y")) {
+           System.out.println("Welcome to Guessing Game.");
+           System.out.println("Guess a number from 1 to 10 (0 to quit): ");
+
+           guess = scan.nextInt();
+           guessNum = 0;
+           while (guess !=0)
+           {
+               guessNum++;
+               if(guess == ans)
+               {
+                   System.out.println("You're on the money!");
+                   break;
+               }
+               else if (guess < ans)
+               {
+                   System.out.println( "Your guess is too low.");
+               }
+               else if (guess > ans)
+               {
+                   System.out.println("Your guess is too high.");
+               }
+               if (guessNum == maxGuesses)
+               {
+                   System.out.println("The number was " + ans);
+                   break;
+               }
+               System.out.println("Enter your guess (0 to quit):");
+               guess = scan.nextInt();
+           }
+           System.out.println("Want to play again? (y/n)");
+           playAgain = scan.next();
+       }
+        System.out.println("Thanks for your participation.");
     }
+
 }
